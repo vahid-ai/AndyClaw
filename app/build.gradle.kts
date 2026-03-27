465d37646f45ad01d7713ded409ee3379f7aa4ca
Markus Haas <haas.markus@protonmail.com>
2026-03-27 16:57:25 +0100

    Bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "org.ethereumphone.andyclaw"
         minSdk = 35
         targetSdk = 36
-        versionCode = 60
+        versionCode = 61
         versionName = versionCode.toString()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
